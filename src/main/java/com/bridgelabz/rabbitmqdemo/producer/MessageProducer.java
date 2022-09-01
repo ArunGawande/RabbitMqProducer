36b2ed8abf916f1a94d3607eaddb2eb48a18f5ac
Arun <arungawande1995@gmail.com>
2022-09-01 15:55:05 +0530

    Added: RabbitMQ -producer

---
+++ src/main/java/com/bridgelabz/rabbitmqdemo/producer/MessageProducer.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.rabbitmqdemo.producer;
+
+import com.bridgelabz.rabbitmqdemo.config.RabbitMqConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MessageProducer {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String message) {
+
+        System.out.println(new Date());
+        rabbitTemplate.convertAndSend(RabbitMqConfig.ROUTING_KEY, message);
+        System.out.println("Is listener returned ::: "+rabbitTemplate.isReturnListener());
+
+    }
+}
